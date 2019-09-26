dfe0858a67c0ebdec1cd5908c398c1aac279f5f6
Gilberto Barraza <gilbarsan@gmail.com>
2019-09-25 23:14:02 -0500

    Primer commit

--- ep_13/webserver/environments/testing/main.tf
+++ ep_13/webserver/environments/testing/main.tf
@@ -1,9 +1,9 @@
 module "webserver" {
   source = "github.com/videocursoscloud/terraform-module-ec2-with-eip?ref=v1.0.2"
-  ami = "ami-00035f41c82244dab"
+  ami = "ami-06f2f779464715dc5"
   instance_type = "t2.micro"
-  key_name = "microserver"
-  vpc_id = "vpc-0913547388f57caa4"
+  key_name = "ec2-amazon01"
+  vpc_id = "vpc-f8ba509c"
   project_name = "webserver"
   environment = "testing"
 }
