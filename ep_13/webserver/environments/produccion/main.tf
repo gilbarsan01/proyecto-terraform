99f6d36258a4c83f2a61cf18ceff588e9486e82c
Gilberto Barraza <gilbarsan@gmail.com>
2019-09-26 00:20:34 -0500

    Se ha solucionado el ep_13

--- ep_13/webserver/environments/produccion/main.tf
+++ ep_13/webserver/environments/produccion/main.tf
@@ -1,9 +1,10 @@
 module "webserver" {
-  source = "github.com/videocursoscloud/terraform-module-ec2-with-eip?ref=v1.0.2"
-  ami = "ami-00035f41c82244dab"
+  source = "github.com/gilbarsan01/terraform-module-ec2-with-eip?ref=v1.0.2"
+  ami = "ami-06f2f779464715dc5"
   instance_type = "t2.micro"
-  key_name = "microserver"
-  vpc_id = "vpc-0913547388f57caa4"
+  key_name = "ec2-amazon01"
+  vpc_id = "vpc-f8ba509c"
+  subnet_id = "subnet-96b995cf"
   project_name = "webserver"
   environment = "produccion"
 }
--- ep_13/webserver/environments/testing/main.tf
+++ ep_13/webserver/environments/testing/main.tf
@@ -1,9 +1,10 @@
 module "webserver" {
-  source = "github.com/videocursoscloud/terraform-module-ec2-with-eip?ref=v1.0.2"
+  source = "github.com/gilbarsan01/terraform-module-ec2-with-eip?ref=v1.0.2"
   ami = "ami-06f2f779464715dc5"
   instance_type = "t2.micro"
   key_name = "ec2-amazon01"
   vpc_id = "vpc-f8ba509c"
+  subnet_id = "subnet-96b995cf"
   project_name = "webserver"
   environment = "testing"
 }
