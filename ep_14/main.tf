4ca7dc2c9723f319b7a65d3ae55cc1636fa2d3fa
Gilberto Barraza <gilbarsan@gmail.com>
2019-09-27 00:13:42 -0500

    Se ha solucionado el ep_14

--- ep_14/main.tf
+++ ep_14/main.tf
@@ -1,11 +1,11 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  version = "1.53.0"
+  version = "2.15.0"
 
   name = "my-vpc"
   cidr = "192.168.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   private_subnets = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
   public_subnets  = ["192.168.10.0/24", "192.168.20.0/24", "192.168.30.0/24"]
 
@@ -18,19 +18,16 @@ module "vpc" {
   }
 }
 
-
 module "webserver" {
-  source  = "videocursoscloud/ec2-with-eip/module"
-  version = "1.0.3"
+  source  = "github.com/gilbarsan01/terraform-module-ec2-with-eip?ref=v1.0.2"
+  #version = "1.0.2"
 
-  ami = "ami-00035f41c82244dab"
+  ami = "ami-06f2f779464715dc5"
   instance_type = "t2.micro"
-  key_name = "microserver"
+  key_name = "ec2-amazon01"
   vpc_id = "${module.vpc.vpc_id}"
+  subnet_id = "${element(module.vpc.public_subnets,0)}"
   project_name = "webserver"
   environment = "testing"
-  subnet_id = "${element(module.vpc.public_subnets,0)}"
+  
 }
-
-
-
