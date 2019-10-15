dfec390d2ec43af72d3d996fbe40d7914eae77fc
Gilberto Barraza <gilbarsan@gmail.com>
2019-10-14 20:29:42 -0500

    Se agrega proyecto enerar-ec2

---
+++ generar-ec2/instancia.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "web" {
+
+        ami = "ami-082b5a644766e0e6f"
+        instance_type = "t2.micro"
+        key_name = "ec2-amazon01"
+
+ vpc_security_group_ids = [ "sg-0e88759d216029be2" ]
+
+}
+
