fcf0db4ef6081ea5bd120f431974a053f20bd465
Gilberto Barraza <gilbarsan@gmail.com>
2019-12-09 23:38:15 -0600

    Se aplican referencias

--- instanciasweb-terraform-aws-asg-and-lc-with-elb_cpu/aws_autoscaling_policy.tf
+++ instanciasweb-terraform-aws-asg-and-lc-with-elb_cpu/aws_autoscaling_policy.tf
@@ -1,3 +1,9 @@
+
+#REFERENCE:https://hands-on.cloud/terraform-recipe-managing-auto-scaling-groups-and-load-balancers/
+#aws_autoscaling_policy declares how AWS should change Auto Scaling Group instances count in case of aws_cloudwatch_metric_alarm.
+#cooldown option is needed to give our infrastructure some time (300 seconds) before increasing Auto Scaling Group once more again.
+#aws_cloudwatch_metric_alarm is very simple alarm, which will be fired, if total CPU utilization of all instances in our Auto Scaling Group will be greater or equal threshold (60% CPU utilization) during 60 seconds.
+
 resource "aws_autoscaling_policy" "web_policy_up" {
   name                   = "web_policy_up"
   scaling_adjustment     = 1
